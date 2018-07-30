480d7f360a49fcbc758056d9d7d31c3ef1ba7d66
freewind <nowindlee@gmail.com>
2018-07-31 01:09:33 +0800

    fix cnzz

--- js/site.js
+++ js/site.js
@@ -3,3 +3,5 @@ for (let i = 0; i < tags.length; i++) {
     let tag = tags[i];
     tag.innerHTML = markdown.toHTML(tag.innerHTML)
 }
+
+var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1274292847'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/z_stat.php%3Fid%3D1274292847' type='text/javascript'%3E%3C/script%3E"));
