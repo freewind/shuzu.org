29e8f00615b4cf83e62b29f179ea492492c6ff03
freewind <nowindlee@gmail.com>
2018-07-31 11:19:58 +0800

    .

--- js/site.js
+++ js/site.js
@@ -1,7 +1,9 @@
-let tags = document.getElementsByClassName("markdown")
-for (let i = 0; i < tags.length; i++) {
+var tags = document.getElementsByClassName("markdown")
+var converter = new showdown.Converter();
+
+for (var i = 0; i < tags.length; i++) {
     let tag = tags[i];
-    tag.innerHTML = markdown.toHTML(tag.innerHTML)
+    tag.innerHTML = converter.makeHtml(tag.innerHTML)
 }
 
 var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1274292847'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/z_stat.php%3Fid%3D1274292847' type='text/javascript'%3E%3C/script%3E"));
