f132e2b8c823f15b03de24cc79b332ab2e687d49
Freewind <nowindlee@gmail.com>
2018-08-28 10:47:42 -0400

    new site with live-search

--- js/site.js
+++ js/site.js
@@ -5,5 +5,3 @@ for (var i = 0; i < tags.length; i++) {
     let tag = tags[i];
     tag.innerHTML = converter.makeHtml(tag.innerHTML)
 }
-
-var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1274292847'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/z_stat.php%3Fid%3D1274292847' type='text/javascript'%3E%3C/script%3E"));
